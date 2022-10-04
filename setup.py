0ccc6d4074f3fc0b3e34a95cd2d7556983423bc6
Jovasa <jovasa46@gmail.com>
2022-10-04 17:39:45 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wanikani_api',
+    version='0.1.0',
+    license="BSD-2-Clause",
+    install_requires=[
+        "pymongo==4.2.0",
+        "urllib3==1.26.12"
+    ],
+    package_dir={'wanikani_api': 'wanikani_api'},
+    packages=["wanikani_api"],
+    py_modules=["wanikani_api"]
+)
